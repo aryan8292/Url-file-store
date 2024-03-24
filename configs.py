a42d8eba3e3df50d31b5809f311201cb27cd2592
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-03-25 03:14:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,43 +1,24 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
+  API_ID = int(os.environ.get("API_ID", "23273287"))
+  API_HASH = os.environ.get("API_HASH", "2c8dbe9c5823a819a25201d7c2ccc3c6")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vku79bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001856581505"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "ziplinker.net")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "a73ab244be8aa742065881d647c98055e0d017b6")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "5079629749"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Aryan6969:Aryan6969@cluster0.krhmwhe.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001764859194")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001856581505"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-  ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
-
-╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
-│
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
-│
-├🔸 Language: [Python 3](https://www.python.org)
-│
-├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
-│
-╰──────[ 😎 ]───────────⍟
-"""
-  ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
- 
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
-"""
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
+  ABOUT_BOT_TEXT = f"✅"
+  ABOUT_DEV_TEXT = f""" 😘 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
